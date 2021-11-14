74df53b4dfa7c840760bd4e8aaada8fd3c3b5b2e
Danil Gusev <ddovarin@gmail.com>
2021-11-14 14:48:42 +0300

    Initial Commit

---
+++ DisplayAlbumiTunes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisplayAlbumiTunes
+//
+//  Created by Данил Гусев on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
